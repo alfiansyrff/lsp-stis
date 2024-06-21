bd334f0fed973d6c9057c1a8b4020c99f7bf8015
alfiansyrff <alfianfurqon27@gmail.com>
2024-06-21 14:51:28 +0700

    minor update

--- src/pages/layout/GuestLayout.jsx
+++ src/pages/layout/GuestLayout.jsx
@@ -8,24 +8,25 @@ import { motion, useScroll } from "framer-motion";
 function GuestLayout({ children }) {
   const [progress, setProgress] = useState(0);
   const [showScrollToTop, setShowScrollToTop] = useState(false);
+  const [loading, setLoading] = useState(true); 
   const { scrollYProgress } = useScroll();
 
-
   useEffect(() => {
-    const fetchData = async () => {
-      setProgress(20);
-      try {
-        await new Promise(resolve => setTimeout(resolve, 2000));
+    const handleReadyStateChange = () => {
+      if (document.readyState === 'complete') {
         setProgress(100);
-      } catch (error) {
-        setProgress(0);
+        setLoading(false); 
       }
     };
 
-    fetchData();
+    // Check if the document is already loaded
+    if (document.readyState === 'complete') {
+      handleReadyStateChange();
+    } else {
+      document.addEventListener('readystatechange', handleReadyStateChange);
+    }
 
     const handleScroll = () => {
-   
       if (window.scrollY > 200) {
         setShowScrollToTop(true);
       } else {
@@ -33,11 +34,10 @@ function GuestLayout({ children }) {
       }
     };
 
-
     window.addEventListener('scroll', handleScroll);
 
-   
     return () => {
+      document.removeEventListener('readystatechange', handleReadyStateChange);
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
@@ -51,28 +51,32 @@ function GuestLayout({ children }) {
 
   return (
     <div className='relative'>
-      
-      <TopLoader progress={progress} setProgress={setProgress} />
-      <Navbar />
-      <div className="content">
-        {children}
-      </div>
-
-      {showScrollToTop && (
-        <motion.div 
-          className='fixed right-7 bottom-10 bg-primaryOrange rounded-full p-1 z-50 hover:cursor-pointer' 
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.3 }}
-          whileHover={{ translateY: -8 }}
-          onClick={scrollToTop}
-        >
-          <KeyboardDoubleArrowUpTwoToneIcon className='text-white' fontSize='large' />
-        </motion.div>
+      {loading ? (
+        <div className="fixed inset-0 bg-black flex items-center justify-center">
+          <TopLoader progress={progress} setProgress={setProgress} />
+        </div>
+      ) : (
+        <>
+          <TopLoader progress={progress} setProgress={setProgress} />
+          <Navbar />
+          <div className="content">
+            {children}
+          </div>
+          {showScrollToTop && (
+            <motion.div 
+              className='fixed right-7 bottom-10 bg-primaryOrange rounded-full p-1 z-50 hover:cursor-pointer' 
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.3 }}
+              whileHover={{ translateY: -8 }}
+              onClick={scrollToTop}
+            >
+              <KeyboardDoubleArrowUpTwoToneIcon className='text-white' fontSize='large' />
+            </motion.div>
+          )}
+          <Footer />
+        </>
       )}
-
-      <Footer />
-      
     </div>
   );
 }
